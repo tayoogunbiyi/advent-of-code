4444aeab09a4a85e0ea312a0f332af3fc798de2c
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-11-28 15:06:52 +0100

    added day1 part b solutions

--- 2019/day1/day_1.go
+++ 2019/day1/day_1.go
@@ -24,8 +24,25 @@ func GetTotalFuelRequirements(rocketMasses []int) int {
 	return totalFuelRequirements
 }
 
+func GetTotalFuelRequirementsPartB(rocketMasses []int) int {
+	totalFuelRequirements := 0
+
+	for _, rocketMass := range rocketMasses {
+		totalFuelRequirements += getFinalFuelFromCurrentFuel(rocketMass)
+	}
+	return totalFuelRequirements
+}
+
+func getFinalFuelFromCurrentFuel(currentFuel int) int {
+	requiredFuel := (currentFuel / 3) - 2
+	if requiredFuel < 0 {
+		return 0
+	}
+	return requiredFuel + getFinalFuelFromCurrentFuel(requiredFuel)
+}
+
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,6 +57,11 @@ func main() {
 
 	result := GetTotalFuelRequirements(masses)
 
-	fmt.Println(result)
+	fmt.Println("Part A result :", result)
+
+	result = GetTotalFuelRequirementsPartB(masses)
+
+	fmt.Println("Part B result :", result)
+
 
 }
--- 2019/day1/day_1_test.go
+++ 2019/day1/day_1_test.go
@@ -23,3 +23,18 @@ func TestGetTotalFuelRequirements(t *testing.T) {
 		}
 	}
 }
+
+func TestGetTotalFuelRequirementsPartB(t *testing.T) {
+	fixtures := []Fixture{
+		{[]int{14}, 2},
+		{[]int{1969}, 966},
+		{[]int{100756}, 50346},
+	}
+
+	for _, fixture := range fixtures {
+		got := GetTotalFuelRequirementsPartB(fixture.RocketMasses)
+		if got != fixture.Expected {
+			t.Errorf("GetTotalFuelRequirementsPartB(%d) = %d; want %d", fixture.RocketMasses, got, fixture.Expected)
+		}
+	}
+}
