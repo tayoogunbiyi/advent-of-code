3c679b3b303618dd6a49ec71fdf4b851569b155f
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-12-06 14:04:04 +0100

    added solutions for 2020/day2 part 2

--- 2020/day2/day2.go
+++ 2020/day2/day2.go
@@ -28,11 +28,13 @@ func readInputIntoPolicySlice(input string) []PasswordPolicy {
 		if len(s) > 0 {
 			occurencesAndLetter, password := strings.Split(s, ":")[0], strings.Split(s, ":")[1]
 			occurences, letter := strings.Split(occurencesAndLetter, " ")[0], strings.Split(occurencesAndLetter, " ")[1]
+
 			minAndMaxOccurences := strings.Split(occurences, "-")
 			minOccurences, _ := strconv.Atoi(minAndMaxOccurences[0])
 			maxOccurences, _ := strconv.Atoi(minAndMaxOccurences[1])
+
 			letter = string(letter)
-			pwPolicy := PasswordPolicy{Password: password, MinOccurences: minOccurences, MaxOccurences: maxOccurences, Letter: letter}
+			pwPolicy := PasswordPolicy{Password: strings.Trim(password, " "), MinOccurences: minOccurences, MaxOccurences: maxOccurences, Letter: letter}
 			passwordPolicies = append(passwordPolicies, pwPolicy)
 		}
 	}
@@ -49,6 +51,20 @@ func CountValidPasswords(passwordPolicies []PasswordPolicy) int {
 	return count
 }
 
+func CountValidPasswordsGivenNewInterpretation(passwordPolicies []PasswordPolicy) int {
+	count := 0
+	for _, pwPolicy := range passwordPolicies {
+		firstIndexContainsLetter := string(pwPolicy.Password[pwPolicy.MinOccurences-1]) == pwPolicy.Letter
+		secondIndexContainsLetter := string(pwPolicy.Password[pwPolicy.MaxOccurences-1]) == pwPolicy.Letter
+		// != simulates the XOR operation in this case as we need only 1 to be true
+		if firstIndexContainsLetter != secondIndexContainsLetter {
+			count++
+		}
+
+	}
+	return count
+}
+
 func main() {
 	data, err := ioutil.ReadFile("input.txt")
 	if err != nil {
@@ -56,4 +72,6 @@ func main() {
 	}
 	passwordPolicies := readInputIntoPolicySlice(string(data))
 	fmt.Printf("%d passwords are valid\n", CountValidPasswords(passwordPolicies))
+	fmt.Printf("%d passwords are valid given new interpretation\n", CountValidPasswordsGivenNewInterpretation(passwordPolicies))
+
 }
--- 2020/day2/day2_test.go
+++ 2020/day2/day2_test.go
@@ -11,3 +11,11 @@ func TestCountValidPasswords(t *testing.T) {
 		t.Errorf("expected CountValidPasswords(%v) = %d; got %d", passwordPolicies, 2, validPasswordCount)
 	}
 }
+
+func TestCountValidPasswordsGivenNewInterpretation(t *testing.T) {
+	passwordPolicies := readInputIntoPolicySlice("1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc")
+	validPasswordCount := CountValidPasswordsGivenNewInterpretation(passwordPolicies)
+	if validPasswordCount != 1 {
+		t.Errorf("expected CountValidPasswords(%v) = %d; got %d", passwordPolicies, 1, validPasswordCount)
+	}
+}
